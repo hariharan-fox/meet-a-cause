d79db1ef9f213e8835a02a2ee1d1da48fb64bcd1
hariharan-fox <hellofoxsocial@gmail.com>
2026-06-28 16:36:28 +0530

    Update index.ts

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,14 +1,15 @@
 import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
 import { getFirestore, type Firestore } from 'firebase/firestore';
+import { getStorage, type FirebaseStorage } from 'firebase/storage';
 import { firebaseConfig } from './config';
 
 export function initializeFirebase() {
   const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
   const auth = getAuth(app);
   const firestore = getFirestore(app);
-
-  return { app, auth, firestore };
+  const storage = getStorage(app);
+  return { app, auth, firestore, storage };
 }
 
 export { FirebaseProvider, useFirebase, useFirebaseApp, useFirestore, useAuth } from './provider';
